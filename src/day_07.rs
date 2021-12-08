6fb72ea961a3e0437bd418ccc679c1c2ad443bfa
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2021-12-08 20:22:00 +1100

    Solved Day 7 Part 2

--- src/day_07.rs
+++ src/day_07.rs
@@ -14,7 +14,7 @@ fn parse_input(input: &str) -> Vec<i64> {
 
 #[aoc(day7, part1)]
 fn solve_part_1(initial_pos: &Vec<i64>) -> i64 {
-    let mut min_fuel: i64 = i64::MAX;
+    let mut min_fuel = i64::MAX;
     // Assuming initial positions have already been sorted
     for target in initial_pos[0]..=initial_pos[initial_pos.len() - 1] {
         // Calculate fuel used to moved to current target position
@@ -29,16 +29,40 @@ fn solve_part_1(initial_pos: &Vec<i64>) -> i64 {
     return min_fuel;
 }
 
+#[aoc(day7, part2)]
+fn solve_part_2(initial_pos: &Vec<i64>) -> i64 {
+    let mut min_fuel = i64::MAX;
+    // Assuming initial positions have already been sorted
+    for target in initial_pos[0]..=initial_pos[initial_pos.len() - 1] {
+        // Use the more expensive fuel calculation method
+        let mut fuel_used = 0;
+        for pos in initial_pos.iter() {
+            let delta = (target - pos).abs();
+            fuel_used += (delta * (delta + 1)) / 2;
+        }
+        if fuel_used < min_fuel {
+            min_fuel = fuel_used;
+        }
+    }
+    return min_fuel;
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
     use std::fs::*;
 
-    // Test cases go here
     #[test]
     fn test_d07_p1_actual() {
         let input = parse_input(&read_to_string("./input/2021/day7.txt").unwrap());
         let result = solve_part_1(&input);
         assert_eq!(347011, result);
+    }
+
+    #[test]
+    fn test_d07_p2_actual() {
+        let input = parse_input(&read_to_string("./input/2021/day7.txt").unwrap());
+        let result = solve_part_2(&input);
+        assert_eq!(98363777, result);
     }
 }
