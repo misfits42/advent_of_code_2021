23f77624a9c6b5925acb5b66373125f672ca7dff
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2021-12-12 20:04:44 +1100

    Generalised Day 12 solution to work for part 2 future implementation

--- src/day_12.rs
+++ src/day_12.rs
@@ -26,9 +26,9 @@ fn parse_input(input: &str) -> HashMap<String, HashSet<String>> {
 fn solve_part_1(cave_graph: &HashMap<String, HashSet<String>>) -> u64 {
     let mut total_paths = 0;
     for sub_cave in cave_graph.get("start").unwrap() {
-        let mut current_path_visited: HashSet<String> = HashSet::new();
-        current_path_visited.insert(String::from("start"));
-        total_paths += visit_nodes(cave_graph, sub_cave, &mut current_path_visited);
+        let mut current_path_visited: HashMap<String, u64> = HashMap::new();
+        current_path_visited.insert(String::from("start"), 1);
+        total_paths += visit_nodes(cave_graph, sub_cave, &mut current_path_visited, 1);
     }
     return total_paths;
 }
@@ -36,22 +36,31 @@ fn solve_part_1(cave_graph: &HashMap<String, HashSet<String>>) -> u64 {
 fn visit_nodes(
     cave_graph: &HashMap<String, HashSet<String>>,
     cave: &String,
-    current_path_visited: &mut HashSet<String>,
+    current_path_visited: &mut HashMap<String, u64>,
+    small_cave_visit_limit: u64,
 ) -> u64 {
     // We have reached the end, so a complete path has been found
     if cave == "end" {
         return 1;
     }
-    // I not at the end, go to each next cave that is not a small cave already visited
+    // If not at the end, go to each next cave that is not a small cave already visited
     let mut total_paths = 0;
     for sub_cave in cave_graph.get(cave).unwrap() {
         // Fork the current path and visit the current cave
         let mut new_visited = current_path_visited.clone();
-        new_visited.insert(cave.to_string());
-        if current_path_visited.contains(sub_cave) && sub_cave.to_ascii_lowercase() == *sub_cave {
+        *new_visited.entry(cave.to_string()).or_insert(0) += 1;
+        if current_path_visited.contains_key(sub_cave)
+            && *current_path_visited.get(sub_cave).unwrap() >= small_cave_visit_limit
+            && sub_cave.to_ascii_lowercase() == *sub_cave
+        {
             continue;
         }
-        total_paths += visit_nodes(cave_graph, sub_cave, &mut new_visited);
+        total_paths += visit_nodes(
+            cave_graph,
+            sub_cave,
+            &mut new_visited,
+            small_cave_visit_limit,
+        );
     }
     return total_paths;
 }
