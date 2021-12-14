6142da2c1ef9223f978192720f88ab06b42ced42
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2021-12-14 17:56:31 +1100

    Solved Day 14 Part 2

--- src/day_14.rs
+++ src/day_14.rs
@@ -33,6 +33,61 @@ fn solve_part_1(polymer_data: &(String, HashMap<String, char>)) -> u64 {
     return calculate_character_max_min_difference(&polymer_template);
 }
 
+#[aoc(day14, part2)]
+fn solve_part_2(polymer_data: &(String, HashMap<String, char>)) -> u64 {
+    // Count number of times each distinct element pair appears
+    let polymer_chars = polymer_data.0.chars().collect::<Vec<char>>();
+    let mut polymer_pairs: HashMap<String, u64> = HashMap::new();
+    for i in 1..polymer_chars.len() {
+        let current_pair = {
+            let mut s = String::new();
+            s.push(polymer_chars[i - 1]);
+            s.push(polymer_chars[i]);
+            s
+        };
+        *polymer_pairs.entry(current_pair).or_insert(0) += 1;
+    }
+    let insertion_rules = &polymer_data.1;
+    // Apply 40 iterations of insertion pair processing
+    for _ in 0..40 {
+        let mut new_polymer_pairs: HashMap<String, u64> = HashMap::new();
+        for (pair, count) in polymer_pairs.into_iter() {
+            if insertion_rules.contains_key(&pair) {
+                let insertion_char = *insertion_rules.get(&pair).unwrap();
+                let old_pair_chars = pair.chars().collect::<Vec<char>>();
+                let mut new_left_pair = String::new();
+                let mut new_right_pair = String::new();
+                new_left_pair.push(old_pair_chars[0]);
+                new_left_pair.push(insertion_char);
+                new_right_pair.push(insertion_char);
+                new_right_pair.push(old_pair_chars[1]);
+                *new_polymer_pairs.entry(new_left_pair).or_insert(0) += count;
+                *new_polymer_pairs.entry(new_right_pair).or_insert(0) += count;
+            } else {
+                *new_polymer_pairs.entry(pair).or_insert(0) += count;
+            }
+        }
+        polymer_pairs = new_polymer_pairs;
+    }
+    // Determine character count
+    let mut char_counts: HashMap<char, u64> = HashMap::new();
+    for (pair, count) in polymer_pairs.iter() {
+        let pair_chars = pair.chars().collect::<Vec<char>>();
+        *char_counts.entry(pair_chars[0]).or_insert(0) += count;
+        *char_counts.entry(pair_chars[1]).or_insert(0) += count;
+    }
+    // Adjust figures for start and end char of original polymer template
+    for (c, count) in char_counts.iter_mut() {
+        *count /= 2;
+        if *c == polymer_chars[0] || *c == polymer_chars[polymer_chars.len() - 1] {
+            *count += 1;
+        }
+    }
+    let max = char_counts.values().max().unwrap();
+    let min = char_counts.values().min().unwrap();
+    return max - min;
+}
+
 /// Applies one iteration of the insertion rules to the given polymer template. Return value is the
 /// resulting polymer template after applying insertion rules.
 fn apply_polymer_insertion_rules(polymer_template: &String, insertion_rules: &HashMap<String, char>) -> String {
@@ -80,5 +135,12 @@ mod test {
         let input = parse_input(&read_to_string("./input/2021/day14.txt").unwrap());
         let result = solve_part_1(&input);
         assert_eq!(2768, result);
+    }
+
+    #[test]
+    fn test_d14_p2_actual() {
+        let input = parse_input(&read_to_string("./input/2021/day14.txt").unwrap());
+        let result = solve_part_2(&input);
+        assert_eq!(2914365137499, result);
     }
 }
