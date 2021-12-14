702ef3ad58a7399c529097eaf7e44293ed3601d8
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2021-12-14 18:07:43 +1100

    Tidied up Day 14 solution to use more space- and time-efficient algorithm

--- src/day_14.rs
+++ src/day_14.rs
@@ -23,20 +23,55 @@ fn parse_input(input: &str) -> (String, HashMap<String, char>) {
 
 #[aoc(day14, part1)]
 fn solve_part_1(polymer_data: &(String, HashMap<String, char>)) -> u64 {
-    let mut polymer_template = polymer_data.0.clone();
-    let insertion_rules = &polymer_data.1;
+    // Count number of times each distinct element pair appears
+    let mut polymer_pairs = determine_polymer_pair_count(&polymer_data.0);
     // Apply 10 iterations of insertion pair processing
     for _ in 0..10 {
-        polymer_template = apply_polymer_insertion_rules(&polymer_template, &insertion_rules);
+        polymer_pairs = apply_polymer_insertion_rules(&polymer_pairs, &polymer_data.1);
     }
-    // Determine difference between maximum and minimum count of elements
-    return calculate_character_max_min_difference(&polymer_template);
+    return calculate_character_max_min_difference(&polymer_pairs, &polymer_data.0);
 }
 
 #[aoc(day14, part2)]
 fn solve_part_2(polymer_data: &(String, HashMap<String, char>)) -> u64 {
     // Count number of times each distinct element pair appears
-    let polymer_chars = polymer_data.0.chars().collect::<Vec<char>>();
+    let mut polymer_pairs = determine_polymer_pair_count(&polymer_data.0);
+    // Apply 40 iterations of insertion pair processing
+    for _ in 0..40 {
+        polymer_pairs = apply_polymer_insertion_rules(&polymer_pairs, &polymer_data.1);
+    }
+    return calculate_character_max_min_difference(&polymer_pairs, &polymer_data.0);
+}
+
+/// Applies one iteration of the insertion rules to the given polymer pairs. Return value is the
+/// resulting polymer pairs after applying insertion rules.
+fn apply_polymer_insertion_rules(
+    polymer_pairs: &HashMap<String, u64>,
+    insertion_rules: &HashMap<String, char>,
+) -> HashMap<String, u64> {
+    let mut new_polymer_pairs: HashMap<String, u64> = HashMap::new();
+    for (pair, count) in polymer_pairs.iter() {
+        if insertion_rules.contains_key(pair) {
+            let insertion_char = *insertion_rules.get(pair).unwrap();
+            let old_pair_chars = pair.chars().collect::<Vec<char>>();
+            let mut new_left_pair = String::new();
+            let mut new_right_pair = String::new();
+            new_left_pair.push(old_pair_chars[0]);
+            new_left_pair.push(insertion_char);
+            new_right_pair.push(insertion_char);
+            new_right_pair.push(old_pair_chars[1]);
+            *new_polymer_pairs.entry(new_left_pair).or_insert(0) += count;
+            *new_polymer_pairs.entry(new_right_pair).or_insert(0) += count;
+        } else {
+            *new_polymer_pairs.entry(pair.clone()).or_insert(0) += count;
+        }
+    }
+    return new_polymer_pairs;
+}
+
+/// Determines the count of each distinct element pair in the given polymer template.
+fn determine_polymer_pair_count(polymer_template: &String) -> HashMap<String, u64> {
+    let polymer_chars = polymer_template.chars().collect::<Vec<char>>();
     let mut polymer_pairs: HashMap<String, u64> = HashMap::new();
     for i in 1..polymer_chars.len() {
         let current_pair = {
@@ -47,29 +82,17 @@ fn solve_part_2(polymer_data: &(String, HashMap<String, char>)) -> u64 {
         };
         *polymer_pairs.entry(current_pair).or_insert(0) += 1;
     }
-    let insertion_rules = &polymer_data.1;
-    // Apply 40 iterations of insertion pair processing
-    for _ in 0..40 {
-        let mut new_polymer_pairs: HashMap<String, u64> = HashMap::new();
-        for (pair, count) in polymer_pairs.into_iter() {
-            if insertion_rules.contains_key(&pair) {
-                let insertion_char = *insertion_rules.get(&pair).unwrap();
-                let old_pair_chars = pair.chars().collect::<Vec<char>>();
-                let mut new_left_pair = String::new();
-                let mut new_right_pair = String::new();
-                new_left_pair.push(old_pair_chars[0]);
-                new_left_pair.push(insertion_char);
-                new_right_pair.push(insertion_char);
-                new_right_pair.push(old_pair_chars[1]);
-                *new_polymer_pairs.entry(new_left_pair).or_insert(0) += count;
-                *new_polymer_pairs.entry(new_right_pair).or_insert(0) += count;
-            } else {
-                *new_polymer_pairs.entry(pair).or_insert(0) += count;
-            }
-        }
-        polymer_pairs = new_polymer_pairs;
-    }
+    return polymer_pairs;
+}
+
+/// Calculates the difference in the counts for the most and least common characters in the given
+/// polymer template.
+fn calculate_character_max_min_difference(
+    polymer_pairs: &HashMap<String, u64>,
+    original_polymer_template: &String,
+) -> u64 {
     // Determine character count
+    let polymer_chars = original_polymer_template.chars().collect::<Vec<char>>();
     let mut char_counts: HashMap<char, u64> = HashMap::new();
     for (pair, count) in polymer_pairs.iter() {
         let pair_chars = pair.chars().collect::<Vec<char>>();
@@ -82,43 +105,6 @@ fn solve_part_2(polymer_data: &(String, HashMap<String, char>)) -> u64 {
         if *c == polymer_chars[0] || *c == polymer_chars[polymer_chars.len() - 1] {
             *count += 1;
         }
-    }
-    let max = char_counts.values().max().unwrap();
-    let min = char_counts.values().min().unwrap();
-    return max - min;
-}
-
-/// Applies one iteration of the insertion rules to the given polymer template. Return value is the
-/// resulting polymer template after applying insertion rules.
-fn apply_polymer_insertion_rules(polymer_template: &String, insertion_rules: &HashMap<String, char>) -> String {
-    let mut new_polymer_template = String::new();
-    let polymer_chars = polymer_template.chars().collect::<Vec<char>>();
-    for i in 1..polymer_chars.len() {
-        // Determine the current character pair within sliding window
-        let current_pair = {
-            let mut s = String::new();
-            s.push(polymer_chars[i - 1]);
-            s.push(polymer_chars[i]);
-            s
-        };
-        // Check if we have a matching insertion pair rule
-        if i == 1 {
-            new_polymer_template.push(polymer_chars[i - 1]);
-        }
-        if insertion_rules.contains_key(&current_pair) {
-            new_polymer_template.push(*insertion_rules.get(&current_pair).unwrap());
-        }
-        new_polymer_template.push(polymer_chars[i]);
-    }
-    return new_polymer_template;
-}
-
-/// Calculates the difference in the counts for the most and least common characters in the given
-/// polymer template.
-fn calculate_character_max_min_difference(polymer_template: &String) -> u64 {
-    let mut char_counts: HashMap<char, u64> = HashMap::new();
-    for c in polymer_template.chars() {
-        *char_counts.entry(c).or_insert(0) += 1;
     }
     let max = char_counts.values().max().unwrap();
     let min = char_counts.values().min().unwrap();
