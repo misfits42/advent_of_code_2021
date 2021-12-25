d48d84ab92ee15622ac0ca75597577dce448015a
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2021-12-25 22:31:44 +1100

    Solved Day 20 Part 2

--- src/day_20.rs
+++ src/day_20.rs
@@ -65,95 +65,132 @@ fn solve_part_1(
     let (mut y_max, mut x_max) = image_input.3;
     let mut default_c = *input_image.get(&(x_min, y_min)).unwrap();
     let mut output_image: HashMap<(i64, i64), char> = HashMap::new();
-    // Apply first iteration
-    for y in y_min..=y_max {
-        for x in x_min..=x_max {
-            // Determine points surrounding current location
-            let pos = (x, y);
-            let mut surround_points: Vec<(i64, i64)> = vec![];
-            for delta_y in -1..=1 {
-                for delta_x in -1..=1 {
-                    surround_points.push((pos.0 + delta_x, pos.1 + delta_y));
+    for _ in 0..2 {
+        // Apply first iteration
+        for y in y_min..=y_max {
+            for x in x_min..=x_max {
+                // Determine points surrounding current location
+                let pos = (x, y);
+                let mut surround_points: Vec<(i64, i64)> = vec![];
+                for delta_y in -1..=1 {
+                    for delta_x in -1..=1 {
+                        surround_points.push((pos.0 + delta_x, pos.1 + delta_y));
+                    }
                 }
+                surround_points.sort_by(|a, b| {
+                    if a.1 < b.1 {
+                        return Ordering::Less;
+                    }
+                    if a.1 == b.1 && a.0 < b.0 {
+                        return Ordering::Less;
+                    }
+                    if a.1 == b.1 && a.0 > b.0 {
+                        return Ordering::Greater;
+                    }
+                    if a.1 > b.1 {
+                        return Ordering::Greater;
+                    }
+                    return Ordering::Equal;
+                });
+                // Determine img enhance alg index
+                let mut index_string = String::new();
+                for pos_s in surround_points.iter() {
+                    let c = *input_image.get(&pos_s).unwrap_or(&default_c);
+                    index_string.push(c);
+                }
+                let index = usize::from_str_radix(&index_string, 2).unwrap();
+                output_image.insert((x, y), img_enhance_alg[index]);
             }
-            surround_points.sort_by(|a, b| {
-                if a.1 < b.1 {
-                    return Ordering::Less;
-                }
-                if a.1 == b.1 && a.0 < b.0 {
-                    return Ordering::Less;
-                }
-                if a.1 == b.1 && a.0 > b.0 {
-                    return Ordering::Greater;
-                }
-                if a.1 > b.1 {
-                    return Ordering::Greater;
-                }
-                return Ordering::Equal;
-            });
-            // Determine img enhance alg index
-            let mut index_string = String::new();
-            for pos_s in surround_points.iter() {
-                let c = *input_image.get(&pos_s).unwrap_or(&default_c);
-                index_string.push(c);
-            }
-            let index = usize::from_str_radix(&index_string, 2).unwrap();
-            output_image.insert((x, y), img_enhance_alg[index]);
         }
-    }
-    // Pad out another layer
-    default_c = *output_image.get(&(x_min, y_min)).unwrap();
-    x_min -= 1;
-    y_min -= 1;
-    x_max += 1;
-    y_max += 1;
-    for x_new in x_min..=x_max {
-        input_image.insert((x_new, y_min), default_c);
-        input_image.insert((x_new, y_max), default_c);
-    }
-    for y_new in y_min..=y_max {
-        input_image.insert((x_min, y_new), default_c);
-        input_image.insert((x_max, y_new), default_c);
-    }
-    // Apply next iteration
-    input_image = output_image;
-    output_image = HashMap::new();
-    for y in y_min..=y_max {
-        for x in x_min..=x_max {
-            // Determine points surrounding current location
-            let pos = (x, y);
-            let mut surround_points: Vec<(i64, i64)> = vec![];
-            for delta_y in -1..=1 {
-                for delta_x in -1..=1 {
-                    surround_points.push((pos.0 + delta_x, pos.1 + delta_y));
-                }
-            }
-            surround_points.sort_by(|a, b| {
-                if a.1 < b.1 {
-                    return Ordering::Less;
-                }
-                if a.1 == b.1 && a.0 < b.0 {
-                    return Ordering::Less;
-                }
-                if a.1 == b.1 && a.0 > b.0 {
-                    return Ordering::Greater;
-                }
-                if a.1 > b.1 {
-                    return Ordering::Greater;
-                }
-                return Ordering::Equal;
-            });
-            // Determine img enhance alg index
-            let mut index_string = String::new();
-            for pos_s in surround_points {
-                let c = *input_image.get(&pos_s).unwrap_or(&default_c);
-                index_string.push(c);
-            }
-            let index = usize::from_str_radix(&index_string, 2).unwrap();
-            output_image.insert((x, y), img_enhance_alg[index]);
+        // Pad out another layer
+        default_c = *output_image.get(&(x_min, y_min)).unwrap();
+        x_min -= 1;
+        y_min -= 1;
+        x_max += 1;
+        y_max += 1;
+        for x_new in x_min..=x_max {
+            input_image.insert((x_new, y_min), default_c);
+            input_image.insert((x_new, y_max), default_c);
         }
+        for y_new in y_min..=y_max {
+            input_image.insert((x_min, y_new), default_c);
+            input_image.insert((x_max, y_new), default_c);
+        }
+        // Apply next iteration
+        input_image = output_image;
+        output_image = HashMap::new();
     }
-    return output_image.values().filter(|x| **x == '1').count();
+    return input_image.values().filter(|x| **x == '1').count();
+}
+
+#[aoc(day20, part2)]
+fn solve_part_2(
+    image_input: &(Vec<char>, HashMap<(i64, i64), char>, (i64, i64), (i64, i64)),
+) -> usize {
+    let img_enhance_alg = image_input.0.clone();
+    let mut input_image = image_input.1.clone();
+    // let mut x_lim = image_input.2;
+    // let mut y_lim = image_input.3;
+    let (mut x_min, mut y_min) = image_input.2;
+    let (mut y_max, mut x_max) = image_input.3;
+    let mut default_c = *input_image.get(&(x_min, y_min)).unwrap();
+    let mut output_image: HashMap<(i64, i64), char> = HashMap::new();
+    for _ in 0..50 {
+        // Apply first iteration
+        for y in y_min..=y_max {
+            for x in x_min..=x_max {
+                // Determine points surrounding current location
+                let pos = (x, y);
+                let mut surround_points: Vec<(i64, i64)> = vec![];
+                for delta_y in -1..=1 {
+                    for delta_x in -1..=1 {
+                        surround_points.push((pos.0 + delta_x, pos.1 + delta_y));
+                    }
+                }
+                surround_points.sort_by(|a, b| {
+                    if a.1 < b.1 {
+                        return Ordering::Less;
+                    }
+                    if a.1 == b.1 && a.0 < b.0 {
+                        return Ordering::Less;
+                    }
+                    if a.1 == b.1 && a.0 > b.0 {
+                        return Ordering::Greater;
+                    }
+                    if a.1 > b.1 {
+                        return Ordering::Greater;
+                    }
+                    return Ordering::Equal;
+                });
+                // Determine img enhance alg index
+                let mut index_string = String::new();
+                for pos_s in surround_points.iter() {
+                    let c = *input_image.get(&pos_s).unwrap_or(&default_c);
+                    index_string.push(c);
+                }
+                let index = usize::from_str_radix(&index_string, 2).unwrap();
+                output_image.insert((x, y), img_enhance_alg[index]);
+            }
+        }
+        // Pad out another layer
+        default_c = *output_image.get(&(x_min, y_min)).unwrap();
+        x_min -= 1;
+        y_min -= 1;
+        x_max += 1;
+        y_max += 1;
+        for x_new in x_min..=x_max {
+            input_image.insert((x_new, y_min), default_c);
+            input_image.insert((x_new, y_max), default_c);
+        }
+        for y_new in y_min..=y_max {
+            input_image.insert((x_min, y_new), default_c);
+            input_image.insert((x_max, y_new), default_c);
+        }
+        // Apply next iteration
+        input_image = output_image;
+        output_image = HashMap::new();
+    }
+    return input_image.values().filter(|x| **x == '1').count();
 }
 
 #[cfg(test)]
@@ -166,5 +203,12 @@ mod test {
         let input = parse_input(&read_to_string("./input/2021/day20.txt").unwrap());
         let result = solve_part_1(&input);
         assert_eq!(5268, result);
+    }
+
+    #[test]
+    fn test_d20_p2_actual() {
+        let input = parse_input(&read_to_string("./input/2021/day20.txt").unwrap());
+        let result = solve_part_2(&input);
+        assert_eq!(16875, result);
     }
 }
