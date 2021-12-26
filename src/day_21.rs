638a54465c7b23b371c17b8897d789ae86e674a7
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2021-12-26 18:56:36 +1100

    Day 21 Part 2 - implemented fully recursive soln candidate
    Not practical to run this due to stack size! Need to find an
    alternative method

--- src/day_21.rs
+++ src/day_21.rs
@@ -5,6 +5,7 @@ struct DeterministicDie {
 }
 
 impl DeterministicDie {
+    /// Creates a new DeterministicDie with starting roll of 1.
     pub fn new() -> Self {
         Self {
             times_rolled: 0,
@@ -12,6 +13,7 @@ impl DeterministicDie {
         }
     }
 
+    /// Rolls the die and rotates the next roll value.
     pub fn roll(&mut self) -> u64 {
         self.times_rolled += 1;
         let this_roll = self.next_roll;
@@ -20,6 +22,29 @@ impl DeterministicDie {
             self.next_roll = 1;
         }
         return this_roll;
+    }
+}
+
+#[derive(Copy, Clone)]
+struct UniverseState {
+    p1_pos: u64,
+    p2_pos: u64,
+    p1_score: u64,
+    p2_score: u64,
+    p1_rolls_this_turn: u64,
+    p2_rolls_this_turn: u64
+}
+
+impl UniverseState {
+    pub fn new(p1_pos: u64, p2_pos: u64) -> Self {
+        Self {
+            p1_pos: p1_pos,
+            p2_pos: p2_pos,
+            p1_score: 0,
+            p2_score: 0,
+            p1_rolls_this_turn: 3,
+            p2_rolls_this_turn: 3,
+        }
     }
 }
 
@@ -72,6 +97,73 @@ fn solve_part_1(player_start_pos: &(u64, u64)) -> u64 {
     } else {
         return p2_score * det_die.times_rolled;
     }
+}
+
+#[aoc(day21, part2)]
+fn solve_part_2(player_start_pos: &(u64, u64)) -> u64 {
+    let universe_state = UniverseState::new(player_start_pos.0, player_start_pos.1);
+    let (p1_wins, p2_wins) = play_turn_dirac(&universe_state);
+    if p1_wins > p2_wins {
+        return p1_wins;
+    } else {
+        return p2_wins;
+    }
+}
+
+fn play_turn_dirac(input_state: &UniverseState) -> (u64, u64) {
+    let mut universe_state = input_state.clone();
+    let mut p1_wins = 0;
+    let mut p2_wins = 0;
+    // Check if a player has won
+    if input_state.p1_score >= 21 {
+        println!(">>>> Player 1 wins: {}", input_state.p1_score);
+        return (1, 0);
+    }
+    if input_state.p2_score >= 21 {
+        println!(">>>> Player 2 wins: {}", input_state.p2_score);
+        return (0, 1);
+    }
+    // P1 turns
+    while universe_state.p1_rolls_this_turn > 0 {
+        universe_state.p1_rolls_this_turn -= 1;
+        for roll in 1..=3 {
+            let mut universe_state_split = universe_state.clone();
+            universe_state_split.p1_pos += roll;
+            if universe_state_split.p1_pos > 10 {
+                universe_state_split.p1_pos = universe_state_split.p1_pos - 10;
+            }
+            if universe_state_split.p1_rolls_this_turn == 0 {
+                universe_state_split.p1_score += universe_state_split.p1_pos;
+            }
+            let (p1_sub_wins, p2_sub_wins) = play_turn_dirac(&universe_state_split);
+            p1_wins += p1_sub_wins;
+            p2_wins += p2_sub_wins;
+        }
+    }
+    // P2 turns
+    while universe_state.p2_rolls_this_turn > 0 {
+        universe_state.p2_rolls_this_turn -= 1;
+        for roll in 1..=3 {
+            let mut universe_state_split = universe_state.clone();
+            universe_state_split.p2_pos += roll;
+            if universe_state_split.p2_pos > 10 {
+                universe_state_split.p2_pos = universe_state_split.p2_pos - 10;
+            }
+            if universe_state_split.p2_rolls_this_turn == 0 {
+                universe_state_split.p2_score += universe_state_split.p2_pos;
+            }
+            let (p1_sub_wins, p2_sub_wins) = play_turn_dirac(&universe_state_split);
+            p1_wins += p1_sub_wins;
+            p2_wins += p2_sub_wins;
+        }
+    }
+    // If no player has won, conduct another turn
+    universe_state.p1_rolls_this_turn = 3;
+    universe_state.p2_rolls_this_turn = 3;
+    let (sub_p1_wins, sub_p2_wins) = play_turn_dirac(&universe_state);
+    p1_wins += sub_p1_wins;
+    p2_wins += sub_p2_wins;
+    return (p1_wins, p2_wins);
 }
 
 #[cfg(test)]
